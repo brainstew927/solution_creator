1559240d6dbd8b6d8b684c0b4618554519ec119f
lorenzo <brainstew927@gmail.com>
2019-03-15 23:31:39 +0100

    initial commit!

---
+++ sol_creator/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace sol_creator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sel_sol = new System.Windows.Forms.ComboBox();
+            this.sel_2 = new System.Windows.Forms.ComboBox();
+            this.input = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // sel_sol
+            // 
+            this.sel_sol.FormattingEnabled = true;
+            this.sel_sol.Location = new System.Drawing.Point(13, 13);
+            this.sel_sol.Name = "sel_sol";
+            this.sel_sol.Size = new System.Drawing.Size(270, 21);
+            this.sel_sol.TabIndex = 0;
+            this.sel_sol.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // sel_2
+            // 
+            this.sel_2.FormattingEnabled = true;
+            this.sel_2.Location = new System.Drawing.Point(290, 13);
+            this.sel_2.Name = "sel_2";
+            this.sel_2.Size = new System.Drawing.Size(271, 21);
+            this.sel_2.TabIndex = 1;
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(13, 53);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(270, 20);
+            this.input.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "testo da immettere:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(290, 53);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(271, 233);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(207, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "aggiungi linea";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 298);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.sel_2);
+            this.Controls.Add(this.sel_sol);
+            this.Name = "Form1";
+            this.Text = "solution creator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox sel_sol;
+        private System.Windows.Forms.ComboBox sel_2;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ sol_creator/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sol_creator
+{
+    public partial class Form1 : Form
+    {
+        String string_sol;
+
+        public string[] split_prob(string problemi)
+        {
+
+            List<string> prob = new List<string>();
+
+            string[] temp;
+
+            temp = problemi.Split(',');
+
+
+            prob.AddRange(temp);
+
+
+            return prob.ToArray();
+
+        }
+
+
+        public string[] split_punt(string[] lett, int sel)
+        {
+
+            List<string> punt = new List<string>();
+
+            string temp;
+
+            foreach (string linea in lett)
+            {
+                temp = linea.Split(':')[sel];
+
+                punt.Add(temp);
+            }
+
+            return punt.ToArray();
+
+        }
+
+        public string[] read()
+        {
+
+            string text = System.IO.File.ReadAllText(@"soluzioni.txt");
+      
+            string[] lines = System.IO.File.ReadAllLines(@"soluzioni.txt");
+
+
+            return lines;
+               
+             
+        }
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            string[] array_str = read();
+
+
+            string[] punt;
+            string[] temp;
+
+            string[][] prob = new string[array_str.Length][];
+
+            int punt_cont = 0;
+
+            punt = split_punt(array_str, 0);
+            temp = split_punt(array_str, 1);
+
+            for (int i =0; i < array_str.Length; i++)
+            {
+               // temp = split_punt(array_str[i], 1);
+               
+
+                prob[i] = split_prob(temp[i]);
+             
+            }
+
+
+            foreach(string pt in punt)
+            {
+                textBox1.AppendText(pt);
+                textBox1.AppendText("\n");
+
+                foreach(string sol in prob[punt_cont])
+                {
+                    textBox1.AppendText("      " + sol);
+                    textBox1.AppendText("\n");
+                }
+
+
+                punt_cont++;
+            }
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text_in = input.Text;
+
+            textBox1.AppendText(text_in);
+            
+        }
+    }
+}
